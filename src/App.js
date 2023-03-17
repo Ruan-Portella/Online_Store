338571c8a46a5f38a6aa1dade7b5419d3d7973ac
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2023-03-17 15:54:21 -0300

    feat: atende requisito 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      productList: [],
+    };
+  }
+
+  render() {
+    const { productList } = this.state;
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={ App } />
+        </Switch>
+        <header className="App-header">
+          <input type="text" placeholder="Digite o que você busca" />
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>Edit src/App.js and save to reload.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <main>
+          {!productList.length > 0 && (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
